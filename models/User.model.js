c8017feec89301331b9f8712e4cb589332c8da54
Filipa <filipamnmlopes@hotmail.com>
2024-06-26 13:12:38 +0200

    adjust User model + add cloudinary middleware

--- models/User.model.js
+++ models/User.model.js
@@ -18,6 +18,14 @@ const userSchema = new Schema(
       type: String,
       required: [true, "Password is required."],
     },
+    userImage: {
+      type: String,
+      default: "https://plus.unsplash.com/premium_photo-1711044006683-a9c3bbcf2f15?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "diverse"]
+    } 
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
