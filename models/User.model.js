fc623ed4d30d98564f3bdd993e42d9f150f38c23
Filipa <filipamnmlopes@hotmail.com>
2024-06-28 15:53:15 +0200

    change default picture of user

--- models/User.model.js
+++ models/User.model.js
@@ -19,8 +19,7 @@ const userSchema = new Schema(
     },
     userImage: {
       type: String,
-      default:
-        "https://plus.unsplash.com/premium_photo-1711044006683-a9c3bbcf2f15?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      default: "https://res.cloudinary.com/dzymhjyvm/image/upload/v1719582689/user_zs74s3.jpg",
     },
     gender: {
       type: String,
