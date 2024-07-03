77c5caa2af9e79983e951e4961480ab4b0d21d44
Filipa <filipamnmlopes@hotmail.com>
2024-07-03 11:49:33 +0200

    add zeros to prices

--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -5,8 +5,7 @@ const User = require("../models/User.model");
 const mergeImages = require("merge-images");
 const { Canvas, Image } = require("canvas");
 const cloudinary = require("cloudinary").v2;
-const {uploadDrawing} = require("../middleware/apiUtils");
-
+const { uploadDrawing } = require("../middleware/apiUtils");
 
 router.post("/", async (req, res, next) => {
   try {
@@ -45,10 +44,10 @@ router.post("/", async (req, res, next) => {
 router.put("/", async (req, res, next) => {
   const price = (product) => {
     const priceMap = {
-      tshirt: 19.5,
-      mug: 7.5,
-      toe_bag: 11.5,
-      beani: 11.3,
+      tshirt: 19.50,
+      mug: 7.50,
+      toe_bag: 11.50,
+      beani: 11.30,
     };
     return priceMap[product];
   };
@@ -85,13 +84,20 @@ router.put("/", async (req, res, next) => {
     originalFile = urlComponents[urlComponents.length - 1];
 
     const resizedDrawing = cloudinary.url(originalFile, {
-      transformation: [{ height: resizedDrawingMeta.height, crop: "fill" }, { fetch_format: "auto" }],
+      transformation: [
+        { height: resizedDrawingMeta.height, crop: "fill" },
+        { fetch_format: "auto" },
+      ],
     });
 
     const b64 = await mergeImages(
       [
         { src: productImg, x: 0, y: 0 },
-        { src: resizedDrawing, x: resizedDrawingMeta.x, y: resizedDrawingMeta.y },
+        {
+          src: resizedDrawing,
+          x: resizedDrawingMeta.x,
+          y: resizedDrawingMeta.y,
+        },
       ],
       { Canvas: Canvas, Image: Image }
     );
@@ -103,7 +109,6 @@ router.put("/", async (req, res, next) => {
       drawing: req.body.drawing, // id
       product: req.body.product,
       price: price(req.body.product),
-      shippingAddress: req.body.shippingAddress,
       mergedImg: mergedImg.secure_url,
     };
 
@@ -118,6 +123,26 @@ router.put("/", async (req, res, next) => {
     res.status(201).json(updatedOrder);
   } catch (error) {
     console.log("There was a problem updating the order: ", error);
+    next(error);
+  }
+});
+
+router.put("/address", async (req, res, next) => {
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(
+      req.body.order,
+      { shippingAddress: req.body.shippingAddress, fulfilled: true },
+      { new: true }
+    );
+    await updatedOrder.save();
+
+    console.log("Shipping address was added to order", updatedOrder);
+    res.status(201).json(updatedOrder);
+  } catch (error) {
+    console.log(
+      "There was a problem updating the address on the order: ",
+      error
+    );
     next(error);
   }
 });
